08e2a4f62d91016e8fd8a6f07fa4f9f7eb5b11b4
minjong kim <alswhddkdl@gmail.com>
2024-12-08 22:36:02 +0900

    [util] TimeZoneMonitor

--- core/data/src/main/kotlin/com/google/samples/apps/nowinandroid/core/data/util/TimeZoneMonitor.kt
+++ core/data/src/main/kotlin/com/google/samples/apps/nowinandroid/core/data/util/TimeZoneMonitor.kt
@@ -48,13 +48,18 @@ import javax.inject.Singleton
  * Utility for reporting current timezone the device has set.
  * It always emits at least once with default setting and then for each TZ change.
  */
+// 추상화
 interface TimeZoneMonitor {
     val currentTimeZone: Flow<TimeZone>
 }
 
+// 실 객체
+// 싱글턴으로 생성
+// 앱 내 전반적으로 사용되기에..
 @Singleton
 internal class TimeZoneBroadcastMonitor @Inject constructor(
     @ApplicationContext private val context: Context,
+    // 애플리케이션 전체 스코프를 보고 있음 -> 전역에서 사용
     @ApplicationScope appScope: CoroutineScope,
     @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
 ) : TimeZoneMonitor {
@@ -62,9 +67,11 @@ internal class TimeZoneBroadcastMonitor @Inject constructor(
     override val currentTimeZone: SharedFlow<TimeZone> =
         callbackFlow {
             // Send the default time zone first.
+            // callback 안에서 flow를 전달
             trySend(TimeZone.currentSystemDefault())
 
             // Registers BroadcastReceiver for the TimeZone changes
+            // 시간대가 변경될 때 사용될 리시버를 등록
             val receiver = object : BroadcastReceiver() {
                 override fun onReceive(context: Context, intent: Intent) {
                     if (intent.action != Intent.ACTION_TIMEZONE_CHANGED) return
@@ -82,11 +89,13 @@ internal class TimeZoneBroadcastMonitor @Inject constructor(
                     }
 
                     // If there isn't a zoneId in the intent, fallback to the systemDefault, which should also reflect the change
+                    // 다시 전달
                     trySend(zoneIdFromIntent ?: TimeZone.currentSystemDefault())
                 }
             }
 
             trace("TimeZoneBroadcastReceiver.register") {
+                // 브로드캐스트 리시버 등록
                 context.registerReceiver(receiver, IntentFilter(Intent.ACTION_TIMEZONE_CHANGED))
             }
 
@@ -94,7 +103,9 @@ internal class TimeZoneBroadcastMonitor @Inject constructor(
             // This way, we can reduce the likelihood that a TZ change wouldn't be caught with the Broadcast Receiver.
             trySend(TimeZone.currentSystemDefault())
 
+            // flow가 종료될 때 실행되는 콜백
             awaitClose {
+                // 브로드캐스트 리시버 해제 (메모리 누수 방지)
                 context.unregisterReceiver(receiver)
             }
         }
@@ -103,5 +114,9 @@ internal class TimeZoneBroadcastMonitor @Inject constructor(
             .conflate()
             .flowOn(ioDispatcher)
             // Sharing the callback to prevent multiple BroadcastReceivers being registered
+            // flow (cold Stream) -> sharedFlow (hot stream) (중간에 중지되도 초기화를 하지 않음)
+            // 단일로 실행되는 업스트림 플로우에서 발행되는 데이터를 여러개의 다운스트림 구독자들이 공유하는 형태
+            // replay 값에 따라 새로운 구독자들에게 값을 다시 발행
+            // WhileSubscribed -> 첫번째 구독자가 생긴 후에 업스트림이 시작, 마지막 구독자가 사라지면 즉시 멈추게 되며, replay 된 캐시를 영원히 보존하게 된다.
             .shareIn(appScope, SharingStarted.WhileSubscribed(5_000), 1)
 }
